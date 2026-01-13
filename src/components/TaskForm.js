d4570cf0a53722cdea02bed157df00a3516df1b2
0Raggis0 <bryanandreslopezarango@gmail.com>
2026-01-13 17:20:53 -0500

    Fix ESLint issues and prepare for Netlify deploy

--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -1,30 +1,39 @@
-import { useState } from "react"
+import { useState } from "react";
 import { useTasks } from "../context/TaskContext";
 
-
 function TaskForm() {
-const [taskName, setTaskName] = useState('')
-const { createTask, adding } = useTasks();
+  const [taskName, setTaskName] = useState("");
+  const { createTask, adding } = useTasks();
 
-const handleSubmit = async e => {  
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    createTask(taskName);  //Llamamos a la funcion createTask del contexto y le pasamos el nombre de la tarea
-    setTaskName('');  //Limpiamos el input despues de crear la tarea
-}
+
+    if (!taskName.trim()) return; // evita tareas vacías
+
+    await createTask(taskName);
+    setTaskName("");
+  };
 
   return (
     <div>
-        <form onSubmit={handleSubmit} className="card card-body">
-            <input type="text" name="taskName" placeholder="Write a task name" className="form-control mb-2" 
-            onChange={e => setTaskName(e.target.value)} value = {taskName}   //Actualizamos el estado taskName con el valor del input
-            />
-            <div className="ms-auto">
-              <button className="btn btn-success" disabled={adding}>{adding ? 'Adding...' : 'Add Task'}</button>
-            </div>         
-        </form>
-        
+      <form onSubmit={handleSubmit} className="card card-body">
+        <input
+          type="text"
+          name="taskName"
+          placeholder="Write a task name"
+          className="form-control mb-2"
+          onChange={(e) => setTaskName(e.target.value)}
+          value={taskName}
+        />
+
+        <div className="ms-auto">
+          <button className="btn btn-success" disabled={adding}>
+            {adding ? "Adding..." : "Add Task"}
+          </button>
+        </div>
+      </form>
     </div>
-  )
+  );
 }
 
-export default TaskForm
+export default TaskForm;
